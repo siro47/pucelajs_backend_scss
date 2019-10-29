f6c39974d9a37e19565ae9e9fbca5989bfd2133d
sramirez <sramirez@solidgeargroup.com>
2019-10-29 10:06:40 +0100

    Added buffer graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import {Socket} from "ngx-socket-io";
+
+import {bufferTime, throttle} from "rxjs/operators";
+import {interval} from "rxjs/internal/observable/interval";
+import {ChartDataSets, ChartOptions} from "chart.js";
+import {Color, Label} from "ng2-charts";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend-scss';
+    lastMessage = 'Ready to receive...';
+    messageCounter = 0;
+
+    public data:number[] = []
+    public lineChartData: ChartDataSets[] = [
+        { data: [], label: 'Messages' },
+    ];
+    public lineChartLabels: Label[] = [];
+    public lineChartOptions: ChartOptions = {
+        responsive: true
+    };
+    public lineChartColors: Color[] = [
+        {
+            borderColor: 'black',
+            backgroundColor: 'rgba(110,132,173,1)',
+        },
+    ];
+    public lineChartLegend = true;
+    public lineChartType = 'bar';
+    public lineChartPlugins = [];
+
+    constructor(private socket: Socket) {
+        for (let i=0; i<100; i++) {
+            this.lineChartLabels.push(i.toString())
+        }
+    }
+
+    ngOnInit() {
+        this.socket.fromEvent('premMessage')
+            .subscribe((msg:string) => this.lastMessage = msg)
+
+        const stdObservable = this.socket.fromEvent('stdMessage')
+
+        const stdObserver1 = stdObservable
+            .pipe(
+                throttle(ev => interval(500))
+            )
+            .subscribe((msg:string) => this.lastMessage = msg)
+
+        const stdObserver2 = stdObservable
+            .pipe(
+                bufferTime(1000)
+            )
+            .subscribe(accMsg => {
+                if (this.data.length >= 100) this.data.shift();
+                this.data.push(accMsg.length)
+                this.lineChartData[0].data = [...this.data]
+            })
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
+import {ChartsModule} from "ng2-charts";
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      SocketIoModule.forRoot(config),
+      ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
